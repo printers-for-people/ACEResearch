ffbbbd938d3ddd9e160a05687145942eaba0df9d
Jookia <contact@jookia.org>
2024-09-08 15:28:59 +1000

    tests: Add cross-compiling

---
+++ tests/build_cross.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if test ! -e armv7l-linux-musleabihf-cross; then
+	echo "Downloading toolchain..."
+	wget https://musl.cc/armv7l-linux-musleabihf-cross.tgz
+	tar -xf armv7l-linux-musleabihf-cross.tgz
+fi
+CC=armv7l-linux-musleabihf-cross/bin/armv7l-linux-musleabihf-gcc
+STRIP=armv7l-linux-musleabihf-cross/bin/armv7l-linux-musleabihf-strip
+$CC -g -Wall -Werror -Wextra -pedantic -std=c11 -static main.c -o main_armv7
+$STRIP main_armv7
