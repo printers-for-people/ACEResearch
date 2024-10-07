bd299a5c0d14c8d36f8d6803fbf4f45c4c95968f
Jookia <contact@jookia.org>
2024-10-07 21:09:17 +1100

    emulator: Add working frame parser implementation

--- emulator/main.py
+++ emulator/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # SPDX-License-Identifier: CC0
 # SPDX-FileCopyrightText: Copyright 2024 Jookia
+
 import asyncio
 import os
 import sys
+import json
 
 
 def realpathFD(fd):
@@ -71,6 +73,83 @@ class SimPTY:
         os.write(self.controlFD, data)
 
 
+class Frame:
+    def __init__(self, payload, crc):
+        self.payload = payload
+        self.crc = crc
+
+
+class Parser:
+    def __init__(self):
+        self.buffer = bytearray()
+        self.field = bytearray()
+        self.state = "none"
+
+        self.payload_length = 0
+        self.payload = bytearray()
+        self.payload_crc = 0
+
+    def add_buffer(self, bytes):
+        self.buffer.extend(bytes)
+
+    def parse_byte(self):
+        byte = self.buffer.pop(0)
+        if self.state == "none":
+            if byte == 0xFF:
+                self.state = "maybe_header"
+        elif self.state == "maybe_header":
+            if byte == 0xAA:
+                self.state = "length"
+                self.field = bytearray()
+                self.payload_length = 0
+                self.payload = bytearray()
+                self.payload_crc = 0
+            else:
+                # TODO: On real hardware can a 0xFF here run back to
+                # maybe_header?
+                self.state = "none"
+        elif self.state == "length":
+            self.field.append(byte)
+            if len(self.field) == 2:
+                length = int.from_bytes(self.field, byteorder="little")
+                self.payload_length = length
+                if self.payload_length == 0:
+                    self.state = "crc"
+                    self.field = bytearray()
+                else:
+                    self.state = "payload"
+        elif self.state == "payload":
+            self.payload.append(byte)
+            if len(self.payload) == self.payload_length:
+                self.state = "crc"
+                self.field = bytearray()
+        elif self.state == "crc":
+            self.field.append(byte)
+            if len(self.field) == 2:
+                crc = int.from_bytes(self.field, byteorder="little")
+                self.payload_crc = crc
+                self.state = "trailer"
+        elif self.state == "trailer":
+            if byte == 0xFE:
+                self.state = "none"
+                frame = Frame(self.payload, self.payload_crc)
+                return frame
+            else:
+                # Deliberately ignore extra bytes
+                pass
+        else:
+            raise RuntimeError("Unknown parser state?")
+        return None
+
+    def parse(self):
+        frames = []
+        while self.buffer != b"":
+            frame = self.parse_byte()
+            if frame is not None:
+                frames.append(frame)
+        return frames
+
+
 async def run_watchdog(watchdog_event):
     try:
         while True:
@@ -81,31 +160,58 @@ async def run_watchdog(watchdog_event):
         return 0
 
 
-async def run_sim(watchdog_event):
+# Calculates CRC-16/MCRF4XX on a bytearray
+def calc_crc(data):
+    crc = 0xFFFF
+
+    for i in range(0, len(data)):
+        byte = data[i]
+        crc = crc ^ byte
+        for i in range(0, 8):
+            if crc & 1:
+                crc = (crc >> 1) ^ 0x8408
+            else:
+                crc = crc >> 1
+
+    return crc
+
+
+def process_frame(frame):
+    crc = calc_crc(frame.payload)
+    if crc != frame.crc:
+        return None
+    payload = {}
+    try:
+        payload = json.loads(frame.payload)
+    except ValueError:
+        return None
+    return b"WE DID IT"
+
+
+async def run_sim(watchdog_event, parser):
     loop = asyncio.get_running_loop()
     sim = SimPTY(loop)
     try:
         while True:
             data = await sim.read()
-            if data == b'\xFF\xAA\x20\x00{"id":140,"method":"get_status"}\x27\xFF\xFE':
+            parser.add_buffer(data)
+            frames = parser.parse()
+            for f in frames:
+                # TODO: Is the watchdog pinged multiple times on real hardware?
+                # TODO: Is the watchdog pinged before or after frame processing?
+                # TODO: Is the watchdog pinged before or after writing?
                 watchdog_event.set()
-                await sim.write(b"OK")
-            elif (
-                data == b'\xFF\xAA\x20\x00{"id":140,"method":"get_statux"}\x27\xFF\xFE'
-            ):
-                watchdog_event.set()
-            elif (
-                data == b'\xFF\xAA\x20\x00{"id":140,"method":"get_status"}\x00\x00\xFE'
-            ):
-                watchdog_event.set()
+                out = process_frame(f)
+                if out is not None:
+                    await sim.write(out)
     finally:
         sim.cleanup()
     return 0
 
 
-async def run_cycle():
+async def run_cycle(parser):
     watchdog_event = asyncio.Event()
-    sim_task = asyncio.create_task(run_sim(watchdog_event))
+    sim_task = asyncio.create_task(run_sim(watchdog_event, parser))
     watchdog_task = asyncio.create_task(run_watchdog(watchdog_event))
     all_tasks = [sim_task, watchdog_task]
     (done, pending) = await asyncio.wait(all_tasks, return_when=asyncio.FIRST_COMPLETED)
@@ -116,10 +222,11 @@ async def run_cycle():
 
 
 async def main():
+    parser = Parser()
     err = 0
     try:
         while True:
-            await run_cycle()
+            await run_cycle(parser)
             await asyncio.sleep(2)
     except (asyncio.CancelledError, KeyboardInterrupt):
         err = 1
